b2c07e005bda5a27967c284561c2f6a3c30e7ae2
Wuming Xie <jedimasterwu@gmail.com>
2017-10-12 19:31:04 -0700

    Initial xcode project files

---
+++ Sweeper/Views/LoginButton.swift
@@ -0,0 +1,21 @@
+//
+//  LoginButton.swift
+//  Sweeper
+//
+//  Created by Wuming Xie on 10/12/17.
+//  Copyright © 2017 team11. All rights reserved.
+//
+
+import UIKit
+
+class LoginButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
