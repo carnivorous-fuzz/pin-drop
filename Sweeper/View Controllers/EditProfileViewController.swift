1038905ba2661cfca7aea03a64ebd083273c9bf9
Paul Sokolik <paul.sokolik@gmail.com>
2017-10-22 22:45:05 -0700

    add profile and profie edit views

---
+++ Sweeper/View Controllers/EditProfileViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EditProfileViewController.swift
+//  Sweeper
+//
+//  Created by Paul Sokolik on 10/22/17.
+//  Copyright © 2017 team11. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
