98977a723a048081654562bbc7edd0e34dd1436e
Paul Sokolik <paul.sokolik@gmail.com>
2017-10-22 21:29:37 -0700

    fix home view toggle

---
+++ Sweeper/View Controllers/HomeViewController.swift
@@ -0,0 +1,65 @@
+//
+//  HomeViewController.swift
+//  Sweeper
+//
+//  Created by Paul Sokolik on 10/22/17.
+//  Copyright © 2017 team11. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    fileprivate enum ViewMode {
+        case list
+        case map
+    }
+    
+    fileprivate var currentViewMode: ViewMode!
+    var listViewController: UIViewController!
+    var mapViewController: UIViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentViewMode = ViewMode.map
+        
+        // instantiate two view controllers we'll toggle between and set default
+        listViewController = UIStoryboard.pinsListVC
+        mapViewController = UIStoryboard.pinsMapVC
+        handleViewTransition(nil, mapViewController, nil)
+    }
+
+    @IBAction func onToggleView(_ sender: UIBarButtonItem) {
+        // toggle image
+        if currentViewMode == ViewMode.map {
+            handleViewTransition(mapViewController, listViewController, .flipFromRight)
+            currentViewMode = ViewMode.list
+            sender.image = UIImage(named: "view-map")
+        } else {
+            handleViewTransition(listViewController, mapViewController, .flipFromLeft)
+            currentViewMode = ViewMode.map
+            sender.image = UIImage(named: "view-list")
+        }
+    }
+    
+    func handleViewTransition(_ fromViewController: UIViewController?, _ toViewController: UIViewController, _ animationStyle: UIViewAnimationTransition?) {
+        
+        // previous view controller removal/cleanup
+        if fromViewController != nil {
+            fromViewController!.willMove(toParentViewController: nil)
+            fromViewController!.view.removeFromSuperview()
+            fromViewController!.removeFromParentViewController()
+        }
+        
+        UIView.beginAnimations("animation", context: nil)
+        UIView.setAnimationDuration(0.5)
+        
+        // newly selected view controller
+        addChildViewController(toViewController)
+        toViewController.view.frame = view.bounds
+        view.addSubview(toViewController.view)
+        toViewController.didMove(toParentViewController: self)
+        UIView.setAnimationTransition(animationStyle ?? .flipFromLeft, for: self.view, cache: false)
+        UIView.commitAnimations()
+    }
+}
