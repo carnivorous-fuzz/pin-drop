558424c0a4185ec9b217b4fc52a22cdb86a52781
Paul Sokolik <paul.sokolik@gmail.com>
2017-10-24 00:28:14 -0700

    add visited pins view

---
+++ Sweeper/Views/ViewedPinCell.swift
@@ -0,0 +1,22 @@
+//
+//  ViewedPinCell.swift
+//  Sweeper
+//
+//  Created by Paul Sokolik on 10/23/17.
+//  Copyright © 2017 team11. All rights reserved.
+//
+
+import UIKit
+
+class ViewedPinCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var pin: Pin! {
+        didSet {
+            if let imageUrl = pin.getImageUrl() {
+                imageView.setImageWith(imageUrl)
+            }
+        }
+    }
+    
+}
