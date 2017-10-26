3ef76c3505d194bcba2adb55a7b79a231b74928b
Wuming Xie <jedimasterwu@gmail.com>
2017-10-25 23:48:33 -0700

    WIP: LiveQuery

--- Sweeper/Services/AppService.swift
+++ Sweeper/Services/AppService.swift
@@ -13,5 +13,27 @@ import ParseLiveQuery
 
 class AppService {
     static let sharedInstance = AppService()
-    static let sharedClient = ParseLiveQuery.Client()
+    
+    let client: Client = Client.shared
+    lazy var pinLikeSubscription: Subscription<PinLike> = {
+        let query = PinLike.query()!.whereKeyExists("user").order(byAscending: "createdAt") as! PFQuery<PinLike>
+        let subscription = self.client.subscribe(query).handleEvent({ (query, event) in
+            switch event {
+            case .created(let pinLike):
+                DispatchQueue.main.async {
+                    self.didLikePin(pinLike)
+                }
+                break
+            default:
+                break
+            }
+        })
+        return subscription
+    }()
+    
+    func didLikePin(_ pinLike: PinLike) {
+        DispatchQueue.main.async {
+            NotificationCenter.default.post(name: NSNotification.Name(pinLike.likedPin!.objectId!), object: nil)
+        }
+    }
 }
